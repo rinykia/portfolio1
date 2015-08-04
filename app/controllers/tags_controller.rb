9f0c8e93399929f05601118288c88d88edc386bf
Natali <rinykia@gmail.com>
2015-08-04 17:02:41 +0300

    add details with bootstrap

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
-	def index
-	  @tags = Tag.order('created_at desc').page(params[:page]).limit(6)
-	end
-	def show 
-  		@tag = Tag.find(params[:id]) 
-  		@destinations = @tag.destinations 
-	end
+  def index
+    @tags = Tag.order('created_at desc').page(params[:page]).limit(6)
+  end
+  def show 
+  	@tag = Tag.find(params[:id]) 
+  	@destinations = @tag.destinations 
+  end
 end
