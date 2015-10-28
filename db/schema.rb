500c36290c3ae2078907b657026429166a25227b
Natali <rinykia@gmail.com>
2015-10-28 15:20:36 +0200

    delete js functions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026123855) do
+ActiveRecord::Schema.define(version: 20151028103537) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -80,9 +80,11 @@ ActiveRecord::Schema.define(version: 20151026123855) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.string   "role",                   default: "user"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
